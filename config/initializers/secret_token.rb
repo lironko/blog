bc42af09f3db77089d605cfd944387ba474c0ccf
Liron <lironbackup@gmail.com>
2014-01-17 21:08:14 +0200

    baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'abb34f45d0065759a235d3e33a2f85fc502dd645ca5287c871cd6bdb87a44cb1b0b01eaa0fbfe7f7ee178c99bec303c7cbcc995b562bb8c4d4e705db7386c066'
